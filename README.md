951d91dbdd98486662ca7872f56137df85f6848b
Dmitriy Kopylenko <dima767@gmail.com>
2014-10-15 10:17:26 -0400

    Added missing ReloadableServicesManager for runtime reloading behavior

--- README.md
+++ README.md
@@ -2,10 +2,11 @@ cas-addon-external-config-services-registry-support
 ===================================================
 
 Foundational classes supporting external configuration facilities for services registries implementations. Such APIs and framework facilities
-include generic external config unmarshalling and generic Spring's application event listener implementation to react to external config changes at runtime.
+include generic external config unmarshalling and generic Spring's application event listener implementation to react to external config changes at runtime
+and ensure that the registered services are then unmarshalled and reloaded into memory for use by other CAS components.
 
 ### Current version
-`1.0.0-RC1`
+`1.0.0-RC2`
 
 ### Supported CAS version
 The minimum supported CAS server version is `4.0.x`
--- src/main/java/net/unicon/cas/addon/serviceregistry/externalconfig/RegisteredServicesExternalConfigSource.java
+++ src/main/java/net/unicon/cas/addon/serviceregistry/externalconfig/RegisteredServicesExternalConfigSource.java
@@ -1,6 +1,7 @@
 package net.unicon.cas.addon.serviceregistry.externalconfig;
 
 import net.unicon.cas.addon.spring.resource.ResourceChangeDetectingEventNotifier;
+import org.jasig.cas.services.ReloadableServicesManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -13,9 +14,11 @@ import org.springframework.core.io.Resource;
  * This class implements dynamic reloading of the contents of the externally changed resource by implementing
  * application listener that reacts to {@link net.unicon.cas.addon.spring.resource.ResourceChangeDetectingEventNotifier
  * .ResourceChangedEvent}s and then delegates the unmarshalling of the external config contents to the configured
- * instance of a {@code RegisteredServicesExternalConfigUnmarshaller}.
+ * instance of a {@code RegisteredServicesExternalConfigUnmarshaller} as well as calls the {@code reload} method on the configured
+ * {@link org.jasig.cas.services.ReloadableServicesManager}.
  *
  * @author Dmitriy Kopylenko
+ * @since 1.0.0
  */
 public class RegisteredServicesExternalConfigSource implements ApplicationListener<ResourceChangeDetectingEventNotifier
         .ResourceChangedEvent> {
@@ -24,13 +27,19 @@ public class RegisteredServicesExternalConfigSource implements ApplicationListen
 
     private RegisteredServicesExternalConfigUnmarshaller<Resource> servicesUnmarshaller;
 
+    private ReloadableServicesManager reloadableServicesManager;
+
+    private final Object mutex = new Object();
+
     protected final static Logger logger = LoggerFactory.getLogger(RegisteredServicesExternalConfigSource.class);
 
     public RegisteredServicesExternalConfigSource(final Resource servicesConfigSource,
-                                                  final RegisteredServicesExternalConfigUnmarshaller<Resource> servicesUnmarshaller) {
+                                                  final RegisteredServicesExternalConfigUnmarshaller<Resource> servicesUnmarshaller,
+                                                  final ReloadableServicesManager reloadableServicesManager) {
 
         this.servicesConfigSource = servicesConfigSource;
         this.servicesUnmarshaller = servicesUnmarshaller;
+        this.reloadableServicesManager = reloadableServicesManager;
         this.servicesUnmarshaller.unmarshalRegisteredServicesFrom(this.servicesConfigSource);
     }
 
@@ -46,8 +55,11 @@ public class RegisteredServicesExternalConfigSource implements ApplicationListen
             logger.error("An exception is caught while trying to access external registered services resource: ", e);
             return;
         }
-        logger.debug("Received change event for external registered services resource {}. Reloading services...",
+        logger.debug("Received changed event for external registered services resource {}. Reloading services...",
                 resourceChangedEvent.getResourceUri());
-        this.servicesUnmarshaller.unmarshalRegisteredServicesFrom(this.servicesConfigSource);
+        synchronized (this.mutex) {
+            this.servicesUnmarshaller.unmarshalRegisteredServicesFrom(this.servicesConfigSource);
+            this.reloadableServicesManager.reload();
+        }
     }
 }
